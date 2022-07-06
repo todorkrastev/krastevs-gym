4d6b5017280e5917680915381647a98df61cda49
beinsaduno <krastevtodordinev@yahoo.com>
2022-07-06 11:25:06 +0200

    Update HTML/CSS files in Gym Project

---
+++ gym/src/main/resources/static/js/index.js
@@ -0,0 +1,12 @@
+/*
+window.addEventListener('load', solve);
+
+function solve() {
+    let btnLog = document.querySelector('#btnLog');
+    btnLog.addEventListener('click', onLog);
+
+    function onLog(event) {
+        document.location.href("https://www.apple.com/");
+    }
+}
+*/
