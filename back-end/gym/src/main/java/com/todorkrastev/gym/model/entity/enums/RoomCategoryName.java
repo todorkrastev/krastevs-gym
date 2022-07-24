6ff7153452d0b37336c6e36aff27111b5961a773
beinsaduno <krastevtodordinev@yahoo.com>
2022-07-24 20:26:00 +0200

    Delete backup project due to mixing with the old one

--- back-end/gym/src/main/java/com/todorkrastev/gym/model/entity/enums/RoomCategoryName.java
+++
@@ -1,9 +0,0 @@
-package com.todorkrastev.gym.model.entity.enums;
-
-public enum RoomCategoryName {
-    R1,
-    R2,
-    R3,
-    R4,
-    R5
-}
--- back-end/gym/src/main/java/com/todorkrastev/gym/repository/PostRepository.java
+++
@@ -1,9 +0,0 @@
-package com.todorkrastev.gym.repository;
-
-import com.todorkrastev.gym.model.entity.Post;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface PostRepository extends JpaRepository<Post, Long> {
-}
